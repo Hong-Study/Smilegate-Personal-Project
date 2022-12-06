4280ea80449e7e50c6c1100548479ec975414466
HongJiHyun <fnvl7855@naver.com>
2022-12-07 02:21:01 +0900

    기초세팅 2

---
+++ MainServer/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+#include "Types.h"
